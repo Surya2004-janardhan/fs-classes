4cad6ef03510b23ebe8ee59a33d9d155080a9fcf
Surya Janardhan <chintalajanardhan2004@gmail.com>
2026-02-16 13:47:17 +0530

    mongodb init

---
+++ mongodb/main.js
@@ -0,0 +1,3 @@
+// lets connect mongodb with nodejs and write raw queries without using any framework and also we ll use all the topics of mongodb like indexing, aggregation, replication, sharding, transactions, etc
+
+const { MongoClient } = require('mongodb');
